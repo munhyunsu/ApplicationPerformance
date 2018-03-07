29cf89dfe5acc59d9e92117e45e295f4d61d05c8
munhyunsu <munhyunsu@gmail.com>
2018-03-07 15:51:47 +0900

    shmwrite

---
+++ shmwrite.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#with open('t', 'w') as f:
+#    for i in range(0, 10000000):
+#        f.write('wow' + str(i))
+
+with open('/dev/shm/t', 'w') as f:
+    for i in range(0, 10000000):
+        f.write('wow' + str(i))
