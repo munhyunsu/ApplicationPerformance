de022e6ba538c40840e55b9cdc40e15253583c65
munhyunsu <munhyunsu@gmail.com>
2018-03-13 09:12:05 +0900

    todo

--- twopersec.py
+++ twopersec.py
@@ -5,6 +5,10 @@ import sys
 def main(argv):
     # TODO(LuHa): check ffmpeg
 
+    # TODO(LuHa): extract 2 image per second
+
+    # TODO(LuHa): calc sim
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
