2d3a34e7d1ee03a179a0e2161fd643bd8859302c
munhyunsu <munhyunsu@gmail.com>
2018-03-25 11:13:05 +0900

    Fix bug

--- MobileUIAutomator/main.py
+++ MobileUIAutomator/main.py
@@ -30,12 +30,13 @@ def main():
 
     # 디렉토리 안에 들어있는 모든  APK파일에 대해서 진행
     for apk in apk_list:
-        try:
-            # APK파일 하나씩 테스트 진행
-            controller.run_test(apk)
-        except Exception as e:
-            # 예외가 발생한다면 앱 테스팅 중지 후 다음 앱 테스트 실행
-            continue
+        controller.run_test(apk)
+        #try:
+        #    # APK파일 하나씩 테스트 진행
+        #    controller.run_test(apk)
+        #except Exception as e:
+        #    # 예외가 발생한다면 앱 테스팅 중지 후 다음 앱 테스트 실행
+        #    continue
 
 if __name__ == '__main__':
     main()
