dc2357138daa3d5f0e4e11c6d6994b46dfd2137f
munhyunsu <munhyunsu@gmail.com>
2018-04-17 10:23:23 +0900

    tcptrace

--- SIAnalyzer/analyze_pcap_rtt.py
+++ SIAnalyzer/analyze_pcap_rtt.py
@@ -24,30 +24,111 @@ def get_files(path, ext = '', recursive = False):
     return path_list
 
 
-def get_rtt_avg(path):
+def get_subprocess_stdout(path):
     cp = subprocess.run(['tcptrace', '-lr', path], 
             stdout = subprocess.PIPE,
             stderr = subprocess.DEVNULL,
             universal_newlines = True)
-    
+
+    return cp.stdout
+
+
+
+def get_rttavg_avg(string):
     try:
         mean = statistics.mean(
                     map(float, 
-                    re.findall('RTT avg:(?:\s)+(\d+.\d+) ms', cp.stdout)))
-        if mean > 0:
-            return mean
+                    re.findall('RTT avg:(?:\s)+(\d+.\d+) ms', string)))
+        return mean
     except:
         return -1
+
+
+def get_idletime_avg(string):
+    try:
+        mean = statistics.mean(
+                    map(float, 
+                    re.findall('idletime max:(?:\s)+(\d+.\d+) ms', 
+                    string)))
+        return mean
+    except:
+        return -1
+
+   
+
+def get_dataxmittime_avg(string):
+    try:
+        mean = statistics.mean(
+                    map(float, 
+                    re.findall('data xmit time:(?:\s)+(\d+.\d+) secs', 
+                    string)))
+        return mean
+    except:
+        return -1
+
+
+
+def get_tcp_len(string):
+    try:
+        length= len(re.findall('TCP connection', 
+                    string))
+        return length
+    except:
+        return -1
+
+
+
+def get_http_len(string):
+    try:
+        length= len(re.findall(':80', 
+                    string))
+        return length
+    except:
+        return -1
+
+
+
+def get_https_len(string):
+    try:
+        length = len(re.findall(':443', 
+                    string))
+        return length
+    except:
+        return -1
+
+
+
+def get_retrans_sum(string):
+    try:
+        sumation = sum(map(int,
+                       re.findall('rexmt data pkts:(?:\s)+(\d+)', 
+                       string)))
+        return sumation
+    except:
+        return -1
+
+
 
 
 def main(argv):
     path = argv[1]
 
+    print('package', 'rtt', 'idletime', 'xmittime', 'tcp', 
+          'http', 'https')
     for path in get_files(path, '.pcap'):
+        string = get_subprocess_stdout(path)
+
         package = (path.split('/')[-1])[:-5]
-        rtt = get_rtt_avg(path)
+        rtt = get_rttavg_avg(string)
+        idletime = get_idletime_avg(string)
+        xmittime = get_dataxmittime_avg(string)
+        tcp = get_tcp_len(string)
+        http = get_http_len(string)
+        https = get_https_len(string)
+        retrans = get_retrans_sum(string)
         if rtt > 0:
-            print(package, rtt, sep = ', ')
+            print(package, rtt, idletime, xmittime, tcp,
+                  http, https, retrans, sep = ', ')
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
