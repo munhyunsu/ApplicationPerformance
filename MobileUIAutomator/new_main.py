e67a9076da3585e672ca878a7c24246fccbcded8
munhyunsu <munhyunsu@gmail.com>
2018-09-03 08:30:12 +0900

    New main

--- MobileUIAutomator/new_main.py
+++ MobileUIAutomator/new_main.py
@@ -9,6 +9,13 @@ import random
 import re
 
 
+def setup_android(path):
+    command = 'adb shell su -c cp {0} /sbin/tcpdump'.format(path)
+    command_check(command)
+    command = 'adb shell su -c chmod +x /sbin/tcpdump'
+    command_check(command)
+
+
 def check_binary(binaries):
     for binary in binaries:
         if shutil.which(binary) is None:
@@ -84,10 +91,17 @@ def parse_xml_log(path):
         if item.get('clickable') == 'true':
             bounds.append(item.get('bounds'))
 
-    choose = random.choice(bounds)
-    axes = re.findall('\d+', choose)
-    point = (random.randrange(int(axes[0]), int(axes[2])),
-             random.randrange(int(axes[1]), int(axes[3])))
+    try:
+        choose = random.choice(bounds)
+        axes = re.findall('\d+', choose)
+        point = (random.randrange(int(axes[0]), int(axes[2])),
+                 random.randrange(int(axes[1]), int(axes[3])))
+    except ValueError:
+        point = (random.randrange(0, 1080),
+                 random.randrange(0, 1920))
+    except IndexError:
+        point = (random.randrange(0, 1080),
+                 random.randrange(0, 1920))
     return size, point
 
 
@@ -104,10 +118,10 @@ def main(argv=sys.argv):
             './output/mp4']
     check_dirs(dirs)
     # Clear env
-    pss = ['tcpdump',
-           'screenrecord']
-    clear_env(pss)
     print('checked all binaries, dirs')
+
+    # Setup android (tcpdump)
+    setup_android('/sdcard/tcpdump')
 
     # Get list of target apps
     if not os.path.exists('app_list.csv'):
@@ -118,108 +132,124 @@ def main(argv=sys.argv):
         for row in reader:
             print(row['package_name'])
             app_list.append(row['package_name'])
-    package_name = app_list[0]
+    # package_name = app_list[0]
 
-    # clear cache without user data
-    # adb shell pm clear APKNAME
-    # adb shell run-as APKNAME rm -rf /data/data/APKNAME/cache/*
-    # adb shell su - c rm - rf /data/data/com.amazon.mShop.android.shopping/cache/*
-    command = 'adb shell su -c rm -rf /data/data/{0}/cache/*'.format(package_name)
-    try:
-        command_check(command)
-    except subprocess.CalledProcessError:
-        pass
-    command = 'adb shell su -c rm /sdcard/*.xml'
-    try:
-        command_check(command)
-    except subprocess.CalledProcessError:
-        pass
-    command = 'adb shell su -c rm /sdcard/*.mp4'
-    try:
-        command_check(command)
-    except subprocess.CalledProcessError:
-        pass
-    command = 'adb shell su -c rm /sdcard/*.pcap'
-    try:
-        command_check(command)
-    except subprocess.CalledProcessError:
-        pass
-    print('removed cache')
-
-    # create XML dir
-    os.makedirs('./output/xml/{0}'.format(package_name),
-                exist_ok=True)
-
-    # execute tcpdump
-    command = 'adb shell su -c tcpdump -i wlan0 -w /sdcard/{0}.pcap'.format(package_name)
-    tcpdump_proc = command_popen(command)
-
-    # execute screenrecord
-    command = 'adb shell screenrecord /sdcard/{0}.mp4'.format(package_name)
-    screenrecord_proc = command_popen(command)
-
-    # launch app
-    start_time = datetime.datetime.now()
-    ping_proc = send_ping()
-    command = 'adb shell monkey -p {0} -c android.intent.category.LAUNCHER 1'.format(package_name)
-    command_check(command)
-    terminate_ping(ping_proc)
-
-    # insert event
-    for index in range(0, 5):
-        size_list = list()
-        # wait for rendering
-        while True:
-            if len(size_list) > 5:
-                size_list.pop(0)
-            if len(size_list) == 5 and len(set(size_list)) == 1:
-                break
-            print('size_list', size_list)
-            # export XML log
-            dump_time = get_second_from_start(start_time)
-            command = 'adb shell uiautomator dump /sdcard/xml/{0}.xml'.format(dump_time)
-            dump_output = command_output(command)
-            print('dump', dump_output)
-            if not dump_output.startswith('UI hierchary dumped to:'):
-                # break
-                # raise Exception
-                size.append(0)
-                point = (random.randrange(0, 1080),
-                         random.randrange(0, 1920))
-                continue
-            # pull XML log
-            command = 'adb pull /sdcard/xml/{0}.xml ./output/xml/{1}/'.format(dump_time, package_name)
+    for package_name in app_list:
+        pss = ['tcpdump',
+               'screenrecord']
+        clear_env(pss)
+        # clear cache without user data
+        # adb shell pm clear APKNAME
+        # adb shell run-as APKNAME rm -rf /data/data/APKNAME/cache/*
+        # adb shell su - c rm - rf /data/data/com.amazon.mShop.android.shopping/cache/*
+        command = 'adb shell su -c rm -rf /data/data/{0}/cache/*'.format(package_name)
+        try:
             command_check(command)
-            last_xml = './output/xml/{0}/{1}.xml'.format(
-                package_name, dump_time)
-            size, point = parse_xml_log(last_xml)
-            size_list.append(size)
-        print('touch', point)
-        command = 'adb shell input tap {0} {1}'.format(point[0], point[1])
+        except subprocess.CalledProcessError:
+            pass
+        command = 'adb shell su -c rm /sdcard/*.xml'
+        try:
+            command_check(command)
+        except subprocess.CalledProcessError:
+            pass
+        command = 'adb shell su -c rm /sdcard/*.mp4'
+        try:
+            command_check(command)
+        except subprocess.CalledProcessError:
+            pass
+        command = 'adb shell su -c rm /sdcard/*.pcap'
+        try:
+            command_check(command)
+        except subprocess.CalledProcessError:
+            pass
+        print('removed cache')
+
+        # create XML dir
+        os.makedirs('./output/xml/{0}'.format(package_name),
+                    exist_ok=True)
+
+        # execute tcpdump
+        command = 'adb shell su -c tcpdump -i wlan0 -w /sdcard/{0}.pcap'.format(package_name)
+        tcpdump_proc = command_popen(command)
+
+        # execute screenrecord
+        command = 'adb shell screenrecord /sdcard/{0}.mp4'.format(package_name)
+        screenrecord_proc = command_popen(command)
+
+        # launch app
+        start_time = datetime.datetime.now()
         ping_proc = send_ping()
+        command = 'adb shell monkey -p {0} -c android.intent.category.LAUNCHER 1'.format(package_name)
         command_check(command)
         terminate_ping(ping_proc)
 
-    # terminate tcpdump
-    tcpdump_proc.terminate()
-    # tcpdump_proc.kill()
+        # insert event
+        for index in range(0, 5):
+            size_list = list()
+            # wait for rendering
+            while True:
+                if len(size_list) > 5:
+                    size_list.pop(0)
+                if len(size_list) == 5 and len(set(size_list)) == 1:
+                    break
+                print('size_list', size_list)
+                # export XML log
+                dump_time = get_second_from_start(start_time)
+                command = 'adb shell uiautomator dump /sdcard/{0}.xml'.format(dump_time)
+                dump_output = command_output(command)
+                print('dump', dump_output)
+                if not dump_output.startswith('UI hierchary dumped to:'):
+                    # break
+                    # raise Exception
+                    size_list.append(0)
+                    point = (random.randrange(0, 1080),
+                             random.randrange(0, 1920))
+                    continue
+                # pull XML log
+                command = 'adb pull /sdcard/{0}.xml ./output/xml/{1}/'.format(dump_time, package_name)
+                command_check(command)
+                last_xml = './output/xml/{0}/{1}.xml'.format(
+                    package_name, dump_time)
+                size, point = parse_xml_log(last_xml)
+                size_list.append(size)
+            print('touch', point)
+            if point[0] != point[1]:
+                command = 'adb shell input tap {0} {1}'.format(point[0], point[1])
+            else:
+                command = 'adb shell input keyevent KEYCODE_BACK'
+            ping_proc = send_ping()
+            command_check(command)
+            terminate_ping(ping_proc)
 
-    # terminate screenrecord
-    screenrecord_proc.terminate()
-    # screenrecord_proc.kill()
+        # stop app
+        ping_proc = send_ping()
+        for index in range(0, 5):
+            command = 'adb shell input keyevent KEYCODE_BACK'
+            command_check(command)
+        command = 'adb shell am force-stop {0}'.format(package_name)
+        command_check(command)
+        terminate_ping(ping_proc)
 
-    # why?
-    pss = ['tcpdump',
-           'screenrecord']
-    terminate_env(pss)
+        # terminate tcpdump
+        tcpdump_proc.terminate()
+        # tcpdump_proc.kill()
 
-    # pull tcpdump
-    command = 'adb pull /sdcard/{0}.pcap ./output/pcap/'.format(package_name)
-    command_check(command)
+        # terminate screenrecord
+        screenrecord_proc.terminate()
+        # screenrecord_proc.kill()
 
-    # pull mp4
-    command = 'adb pull /sdcard/{0}.mp4 ./output/mp4/'.format(package_name)
-    command_check(command)
+        # why?
+        pss = ['tcpdump',
+               'screenrecord']
+        terminate_env(pss)
+
+        # pull tcpdump
+        command = 'adb pull /sdcard/{0}.pcap ./output/pcap/'.format(package_name)
+        command_check(command)
+
+        # pull mp4
+        command = 'adb pull /sdcard/{0}.mp4 ./output/mp4/'.format(package_name)
+        command_check(command)
 
 
 if __name__ == '__main__':
