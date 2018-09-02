789271100b69a832856085cfc1de69d992906e02
Hyun-Su, Mun <munhyunsu@gmail.com>
2018-09-03 05:53:35 +0900

    experiment POC

--- MobileUIAutomator/new_main.py
+++ MobileUIAutomator/new_main.py
@@ -15,6 +15,35 @@ def check_binary(binaries):
             raise FileNotFoundError
 
 
+def check_dirs(dirs):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+
+
+def clear_env(pss):
+    for ps in pss:
+        command = 'adb shell "ps | grep {0}"'.format(ps)
+        try:
+            output = command_output(command)
+        except subprocess.CalledProcessError:
+            continue
+        psnum = re.findall('\d+', output)[0]
+        command = 'adb shell su -c kill -9 {0}'.format(psnum)
+        command_check(command)
+
+
+def terminate_env(pss):
+    for ps in pss:
+        command = 'adb shell "ps | grep {0}"'.format(ps)
+        try:
+            output = command_output(command)
+        except subprocess.CalledProcessError:
+            continue
+        psnum = re.findall('\d+', output)[0]
+        command = 'adb shell su -c kill -2 {0}'.format(psnum)
+        command_check(command)
+
+
 def command_popen(command):
     return subprocess.Popen(command, shell=True)
 
@@ -41,7 +70,7 @@ def terminate_ping(ping_proc):
 
 
 def get_second_from_start(start_time):
-    return (start_time - datetime.datetime.now()).total_seconds()
+    return abs(int((start_time - datetime.datetime.now()).total_seconds()))
 
 
 def parse_xml_log(path):
@@ -69,6 +98,16 @@ def main(argv=sys.argv):
     # Check binaries
     binaries = ['adb']
     check_binary(binaries)
+    # Check dirs
+    dirs = ['./output/xml',
+            './output/pcap',
+            './output/mp4']
+    check_dirs(dirs)
+    # Clear env
+    pss = ['tcpdump',
+           'screenrecord']
+    clear_env(pss)
+    print('checked all binaries, dirs')
 
     # Get list of target apps
     if not os.path.exists('app_list.csv'):
@@ -86,10 +125,33 @@ def main(argv=sys.argv):
     # adb shell run-as APKNAME rm -rf /data/data/APKNAME/cache/*
     # adb shell su - c rm - rf /data/data/com.amazon.mShop.android.shopping/cache/*
     command = 'adb shell su -c rm -rf /data/data/{0}/cache/*'.format(package_name)
-    command_check(command)
+    try:
+        command_check(command)
+    except subprocess.CalledProcessError:
+        pass
+    command = 'adb shell su -c rm /sdcard/*.xml'
+    try:
+        command_check(command)
+    except subprocess.CalledProcessError:
+        pass
+    command = 'adb shell su -c rm /sdcard/*.mp4'
+    try:
+        command_check(command)
+    except subprocess.CalledProcessError:
+        pass
+    command = 'adb shell su -c rm /sdcard/*.pcap'
+    try:
+        command_check(command)
+    except subprocess.CalledProcessError:
+        pass
+    print('removed cache')
+
+    # create XML dir
+    os.makedirs('./output/xml/{0}'.format(package_name),
+                exist_ok=True)
 
     # execute tcpdump
-    command = 'adb shell su -c tcpdump -i wlan0 -w /sdcard/{0}.pcap -s 0'.format(package_name)
+    command = 'adb shell su -c tcpdump -i wlan0 -w /sdcard/{0}.pcap'.format(package_name)
     tcpdump_proc = command_popen(command)
 
     # execute screenrecord
@@ -105,25 +167,60 @@ def main(argv=sys.argv):
 
     # insert event
     for index in range(0, 5):
-        # export XML log
-        dump_time = get_second_from_start(start_time)
-        command = 'adb shell uiautomator dump /sdcard/xml/{0}.xml'.format(dump_time)
-        dump_output = command_output(command)
-        if not dump_output.startswith('UI hierchary dumped to:'):
-            continue
-        # pull XML log
-        command = 'adb pull /sdcard/xml/{0}.xml ./{1}/xml/'.format(dump_time, package_name)
+        size_list = list()
+        # wait for rendering
+        while True:
+            if len(size_list) > 5:
+                size_list.pop(0)
+            if len(size_list) == 5 and len(set(size_list)) == 1:
+                break
+            print('size_list', size_list)
+            # export XML log
+            dump_time = get_second_from_start(start_time)
+            command = 'adb shell uiautomator dump /sdcard/xml/{0}.xml'.format(dump_time)
+            dump_output = command_output(command)
+            print('dump', dump_output)
+            if not dump_output.startswith('UI hierchary dumped to:'):
+                # break
+                # raise Exception
+                size.append(0)
+                point = (random.randrange(0, 1080),
+                         random.randrange(0, 1920))
+                continue
+            # pull XML log
+            command = 'adb pull /sdcard/xml/{0}.xml ./output/xml/{1}/'.format(dump_time, package_name)
+            command_check(command)
+            last_xml = './output/xml/{0}/{1}.xml'.format(
+                package_name, dump_time)
+            size, point = parse_xml_log(last_xml)
+            size_list.append(size)
+        print('touch', point)
+        command = 'adb shell input tap {0} {1}'.format(point[0], point[1])
+        ping_proc = send_ping()
         command_check(command)
-
-
-
-
+        terminate_ping(ping_proc)
 
     # terminate tcpdump
     tcpdump_proc.terminate()
+    # tcpdump_proc.kill()
 
     # terminate screenrecord
     screenrecord_proc.terminate()
+    # screenrecord_proc.kill()
+
+    # why?
+    pss = ['tcpdump',
+           'screenrecord']
+    terminate_env(pss)
+
+    # pull tcpdump
+    command = 'adb pull /sdcard/{0}.pcap ./output/pcap/'.format(package_name)
+    command_check(command)
+
+    # pull mp4
+    command = 'adb pull /sdcard/{0}.mp4 ./output/mp4/'.format(package_name)
+    command_check(command)
+
 
 if __name__ == '__main__':
     main()
