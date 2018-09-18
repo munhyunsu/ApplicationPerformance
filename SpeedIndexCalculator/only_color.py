336207121455ae626aaa819d456f296e4c805200
munhyunsu <munhyunsu@gmail.com>
2018-09-18 10:42:11 +0900

    mid result

--- SpeedIndexCalculator/only_color.py
+++ SpeedIndexCalculator/only_color.py
@@ -91,7 +91,11 @@ def mp4_to_jpgs(mp4_dir):
 
 
 def main(argv = sys.argv):
+    '''Need one arguments
+    arg1 = input path
+    '''
 #    mp4_to_jpgs(argv[1])
+    # mid results
     for app_dir in get_app_dirs(argv[1]):
         check_thresholds(app_dir)
 
