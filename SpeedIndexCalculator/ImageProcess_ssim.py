82d1f8cd1d7c6976ae7665fa88077153934a47c9
munhyunsu <munhyunsu@gmail.com>
2018-12-10 11:01:46 +0900

    runtime

--- SpeedIndexCalculator/ImageProcess_ssim.py
+++ SpeedIndexCalculator/ImageProcess_ssim.py
@@ -9,10 +9,13 @@ import scipy.integrate as integrate
 import sys
 import logging
 import logging.config
+import xml.etree.ElementTree as ET
+from operator import itemgetter
 
-INPUTDIR = '/home/harny/SharedFolder/Mobile/180415/record2/'
-INPUTDIR2 = '/home/harny/SharedFolder/Mobile/180415/record3/'
-OUTPUTDIR = '/home/harny/SharedFolder/Mobile/180415/video2/'
+XMLDIR = '/home/harny/Github/ApplicationPerformance/MobileUIAutomator/a5-wlan/xml/'
+MP4DIR = '/home/harny/Github/ApplicationPerformance/MobileUIAutomator/a5-wlan/mp4/'
+TEMPDIR = './temp/'
+OUTPUTDIR = './output/'
 
 
 def color_based(image1, image2):
@@ -41,26 +44,23 @@ def color_based(image1, image2):
     return average_similarity
 
 
-def get_split_point(input_list):
+def get_split_point(input_list, cut_point):
 
-    list_split_point = []
-    temp = 0
-    temp_index = 0
-    for index in range(len(input_list)):
-        if input_list[index][2] >= 0.8:
-            if input_list[index][2] >= temp:
-                temp = input_list[index][2]
-                temp_index = index
+    list_split_point = list()
+    for index in range(0, len(cut_point)):
+        from_index = 0
+        if index > 0:
+            from_index = cut_point[index-1]*10
+        to_index = cut_point[index]*10
+
+        for index in range(to_index, from_index, -1):
+            if input_list[index][2] >= 0.8:
+                continue
             else:
-                temp = 0.8
-                temp_index = index
-
-        else:
-            if temp != 0:
-                list_split_point.append(temp_index)
-                temp = 0
-    list_split_point.append(len(input_list)-1)
+                list_split_point.append((from_index, index))
+                break
     return list_split_point
+
 
 def get_speed_index_of_first_activity(list_similarity):
     # 리스트를 역순으로 뒤집어 액티비티 로딩 완료 후 움직이지 않는 스크린샷 제거
@@ -84,22 +84,15 @@ def get_speed_index_of_first_activity(list_similarity):
 
     return [[speed_index_list, speed_index]]
 
+
 def get_speed_index(list_similarity, list_split_point):
-    intevel = 1
-    latest_index = 0
-    list_speed = []
-    sum_of_speed = 0
-    for split_point in list_split_point:
-        speed_index = []
-        for index in range(latest_index, split_point+1):
-            if(list_similarity[index][2] >= 0.8): #남은 여유시간 제거
-                continue
-            speed_index.append(list_similarity[index][1].split('.jpg')[0][-3:])
-            sum_of_speed += intevel * (1 - list_similarity[index][2])
-        list_speed.append([speed_index, sum_of_speed])
-        sum_of_speed = 0
-        latest_index = split_point+1
-    return list_speed
+    speed_index = list()
+    for from_index, to_index in list_split_point:
+        speed = 0
+        for sim in list_similarity[from_index:to_index]:
+            speed = speed + sim
+        speed_index.append(speed)
+    return speed_index
 
 
 def run_ffmpeg(video_name):
@@ -108,16 +101,16 @@ def run_ffmpeg(video_name):
     """
     # mp4파일 이름으로 디렉토리 생성
     try:
-        os.makedirs(OUTPUTDIR + str(video_name), exist_ok = True)
+        os.makedirs(TEMPDIR + str(video_name), exist_ok = True)
     except FileExistsError as e:
-        print (e)
+        print(e)
     except Exception as e:
         print(e)
         raise e
 
     # ffmpeg 실행
     # LuHa: fps=2 로 변경
-    command = 'ffmpeg -i ' + INPUTDIR+str(video_name) + '.mp4 -vf fps=10 ' + OUTPUTDIR+str(video_name) + '/out%03d.jpg'
+    command = 'ffmpeg -i ' + MP4DIR+str(video_name) + '.mp4 -vf fps=10 ' + TEMPDIR+str(video_name) + '/out%04d.jpg'
     try:
         ffmpeg = subprocess.check_call(command, stdout=subprocess.PIPE, shell=True)
     except Exception as e:
@@ -133,6 +126,7 @@ def list_mp4(path):
         if f.endswith('.mp4'):
             result.append(f.split('.mp4')[0])
     return result
+
 
 def list_jpg(path):
     result = []
@@ -154,9 +148,10 @@ def get_similarity_list(files_list):
         except IndexError as e:
             continue
         similarity = ssim(image1, image2, multichannel = True)
-        list_similarity.append([files_list[index], files_list[index+1], similarity])
+        list_similarity.append((files_list[index], files_list[index+1], similarity))
 
     return list_similarity
+
 
 def get_similarity_list_last(files_list):
     files_list = files_list[:5]
@@ -176,8 +171,6 @@ def get_similarity_list_last(files_list):
     return list_similarity
 
 
-
-
 def write2csv(app_name, speed_list):
     if len(speed_list) == 0:
         logging.info(app_name + ' len(speed_list) is 0')
@@ -193,12 +186,67 @@ def write2csv(app_name, speed_list):
 
     csv.close()
 
+
+def parse_xml_log(path):
+    tree = ET.parse(path)
+    root = tree.getroot()
+    it = root.iter()
+    size = 0
+    bounds = list()
+    for item in it:
+        size = size+1
+    return size
+
+
+def get_cut_point(path):
+    dir_queue = list()
+
+    with os.scandir(path) as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_dir():
+                dir_queue.append(entry.path)
+
+    cut_point = dict()
+    for inner_path in dir_queue:
+        cuts_t = list()
+        with os.scandir(inner_path) as it:
+            for entry in it:
+                if not entry.name.startswith('.') and entry.is_file():
+                    sec = int((entry.name.split('/')[-1]).split('.')[0])
+                    ln = parse_xml_log(entry.path)
+                    cuts_t.append((sec, ln))
+        cuts_t.sort(key=itemgetter(0))
+
+        cuts = list()
+        dup = 0
+        dup_c = 0
+        for sec, ln in cuts_t:
+            if dup == ln:
+                dup_c = dup_c + 1
+            else:
+                dup_c = 0
+            dup = ln
+            if dup_c >= 4:
+                cuts.append(sec)
+                dup_c = 0
+            if (len(cuts)+1)*60 < sec:
+                cuts.append(sec)
+            if len(cuts) >= 5:
+                break
+
+        cut_point[inner_path.split('/')[-1]] = cuts
+
+    return cut_point
+
+
 def main():
 
-    mp4_list = list_mp4(INPUTDIR)
+    mp4_list = list_mp4(MP4DIR)
 
-    os.makedirs(INPUTDIR2, exist_ok = True)
+    os.makedirs(TEMPDIR, exist_ok = True)
     os.makedirs(OUTPUTDIR, exist_ok = True)
+
+    cut_point = get_cut_point(XMLDIR)
 
     for video_name in mp4_list:
         try:
@@ -207,29 +255,13 @@ def main():
         except Exception as e:
             logging.error(video_name + ' : run_ffmpeg error')
             continue
-
-        files_list = list_jpg(OUTPUTDIR+video_name+'/')
+        files_list = list_jpg(TEMPDIR+video_name+'/')
         files_list.sort()
 
-        """
         list_similarity = get_similarity_list(files_list)
-        logging.info(video_name + ' list_similarity : ' + list_similarity)
-        list_split_point = get_split_point(list_similarity)
-        logging.info(video_name + ' list_split_point : ' + list_split_point)
+        list_split_point = get_split_point(list_similarity, cut_point[video_name.split('.')[0]])
         speed_list = get_speed_index(list_similarity, list_split_point)
-        logging.info(video_name + ' speed_list : ' + speed_list)
-        write2csv(video_name, speed_list)
-        """
-
-        list_similarity = get_similarity_list(files_list)
-        # list_similarity_last = get_similarity_list_last(files_list)
-        # print(list_similarity_last)
-        logging.info(video_name + ' list_similarity : ' + str(list_similarity))
-        speed_list = get_speed_index_of_first_activity(list_similarity)
-        logging.info(video_name + ' speed_list : ' + str(speed_list))
-        write2csv(video_name, speed_list)
-
-        os.rename(INPUTDIR+video_name+'.mp4', INPUTDIR2+video_name+'.mp4')
+        print(video_name, list_split_point, speed_list)
 
 
 if __name__ == '__main__':
