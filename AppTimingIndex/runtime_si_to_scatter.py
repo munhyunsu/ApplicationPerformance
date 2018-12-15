7fa9fa39cccc3b94c18b00f06c7f0f74cdcd5067
munhyunsu <munhyunsu@gmail.com>
2018-12-15 11:41:21 +0900

    mean

--- AppTimingIndex/runtime_si_to_scatter.py
+++ AppTimingIndex/runtime_si_to_scatter.py
@@ -25,23 +25,24 @@ import statistics
 #        for entry in runtime:
 #            writer.writerow((rank.index(entry[0])+1, entry[0], entry[1]))
 
+
 def main(argv):
-    with open(argv[1], 'r') as f:
-        reader = csv.reader(f)
-        app = dict()
-        runtime = dict()
-        count = 1
-        rank = list()
-        for row in reader:
-            if row[0] not in app.keys():
-                count = count + 1
-                app[row[0]] = (row[2], count)
-            elif float(row[2]) >= 100:
-                runs = runtime.get(row[0], list())
-                runs.append(float(row[2]))
-                runtime[row[0]] = runs
-    with open(argv[2], 'w') as f:
-        rank.sort()
+    app = dict()
+    runtime = dict()
+    for path in argv[1:-1]:
+        with open(path, 'r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if eval(row[1])[0] == 0:
+                    if row[0] not in app.keys():
+                        app[row[0]] = row[2]
+                    elif app[row[0]] > row[2]:
+                        app[row[0]] = row[2]
+                elif float(row[2]) >= 100:
+                    runs = runtime.get(row[0], list())
+                    runs.append(float(row[2]))
+                    runtime[row[0]] = runs
+    with open(argv[-1], 'w') as f:
         writer = csv.writer(f)
         for key in runtime.keys():
             writer.writerow((key, app[key][0], statistics.mean(runtime[key])))
@@ -51,4 +52,3 @@ if __name__ == '__main__':
     import sys
     print(sys.argv)
     sys.exit(main(sys.argv))
-    
