05408d3a19a1efad82daad6a11695f87cda446ac
munhyunsu <munhyunsu@gmail.com>
2019-02-09 13:58:19 +0900

    test server

--- start_test_server.py
+++ start_test_server.py
@@ -1,15 +1,22 @@
+import os
 import sys
 
 from src.client_ui import ClientUI
-
+#from src.apk_downloader import APKDownloader
 
 ARGS = None
 
 
 def main():
     ui = ClientUI()
+    if ARGS.driver is None:
+        ARGS.driver = ui.query_args('driver')
+    ARGS.driver = os.path.abspath(os.path.expanduser(ARGS.driver))
     if ARGS.package is None:
-        ARGS.package = ui.query_package()
+        ARGS.package = ui.query_args('package')
+
+    print('ARGS', ARGS)
+#    downloader = APKDownloader(ARGS.driver)
 
 
 if __name__ == '__main__':
@@ -17,7 +24,12 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--package',
+                        type=str,
                         help='test package name')
+    parser.add_argument('-d', '--driver',
+                        type=str,
+                        default='./webdriver/chromedriver',
+                        help='web driver path')
 
     ARGS = parser.parse_args()
 
