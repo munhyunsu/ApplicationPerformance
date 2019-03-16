498c863a49b3c4157fa79dc712578a2f86206047
munhyunsu <munhyunsu@gmail.com>
2019-03-16 11:16:51 +0900

    Args

--- device_manager/dmanager.py
+++ device_manager/dmanager.py
@@ -1,12 +1,19 @@
 import sys
 
 
+ARGS = None
+
 def main():
-    pass
+    print(ARGS)
 
 
 if __name__ == '__main__':
     import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device', type=str,
+                        help='device model for creation')
+    parser.parse_args()
 
     main()
 
