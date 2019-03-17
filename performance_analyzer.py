d2dc5563451cb559b6c5b646aaa075dad9268018
munhyunsu <munhyunsu@gmail.com>
2019-03-17 23:34:50 +0900

    arguments parser

--- performance_analyzer.py
+++ performance_analyzer.py
@@ -1,5 +1,6 @@
 import sys
 
+from util.path import std_path
 
 ARGS = None
 
@@ -10,11 +11,20 @@ def main():
 if __name__ == '__main__':
     import argparse
 
+    # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-iv', '--input-video', type=str, required=True,
-                         help='input video directory path')
+                        help='input video directory path')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='output directory path')
 
     ARGS = parser.parse_args()
+
+    # Preprocessing arguments
+    if 'input_video' in ARGS:
+        ARGS.input_video = std_path(ARGS.input_video)
+    if 'output' in ARGS:
+        ARGS.output = std_path(ARGS.output)
 
     main()
     
