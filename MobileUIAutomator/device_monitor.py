03425d237a4ddbe385c977af455ecdadf6fd20b3
munhyunsu <munhyunsu@gmail.com>
2019-08-04 14:36:56 +0900

    output dir

--- MobileUIAutomator/device_monitor.py
+++ MobileUIAutomator/device_monitor.py
@@ -1,6 +1,7 @@
 class DeviceMonitor(object):
-    def __init__(self, adb_path):
+    def __init__(self, adb_path, output_path):
         self.adb_path = adb_path
+        self.output_path = output_path
 
     def do_monitor(self):
         pass
